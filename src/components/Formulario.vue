e45aa07cb900b0803d68de5cc614d5c806c4fa78
Juan Carlos Condori <juanc.com@gmail.com>
2020-05-26 16:45:37 -0400

    Adición de formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,12 +1,47 @@
 <template>
   <div>
-    <h1>Formulario de Contacto</h1>
+    <form>
+      <div class="row">
+        <div class="col-xs12 col-sm-8 col-md-6" >
+          <h1 class="text-center">Formulario de Contacto</h1>
+          <hr>
+          <div class="form-group">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" class="form-control" v-model='nombre'>
+          </div>
+          <div clas="form-group">
+            <label for="email">Email:</label>
+            <input type="text" id="email" class="form-control" v-model='email'>
+          </div>
+        </div>
+      </div>
+    </form>
+    <div class="row">
+      <div class="col-xs12 col-sm-8 col-md-6">
+        <hr>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="text-center">Datos</h3>
+          </div>
+          <div class="panel-body">
+            <p>Nombre: {{nombre}}</p>
+            <p>Email: {{email}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Formulario'
+  name: 'Formulario',
+  data(){
+    return{
+      nombre:'',
+      email:''
+    }
+  }
 }
 </script>
 
